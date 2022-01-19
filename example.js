6a0900c4a1fb2e344a98742a8a2a398c9d785c0d
FNC <fncolon@pm.me>
2022-01-19 14:51:57 +0700

    update compile result

--- example.js
+++ example.js
@@ -4,6 +4,8 @@ import fs from "fs";
 const sharp = fs.readFileSync("./index.sharp", {
   encoding: "utf8",
 });
-const { value } = compile(sharp);
+const value = compile(sharp);
 
-console.log(value);
+fs.writeFileSync("./compile_result.txt", value, {
+  encoding: "utf8",
+});
